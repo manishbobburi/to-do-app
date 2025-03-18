66a46205e57b7032445f495793f3803f4fd91c96
MANISH6148 <manishbobburi6148@gmail.com>
2025-03-18 10:30:29 +0530

    Add function to handle task creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import { useState, useEffect, useCallback, useId} from "react"
+import { useState, useEffect, useCallback, useId} from "react";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if(task.trim != "") {
+      setTasks([...tasks,task]);
+      setTask("");
+    }
+  }
 
   return (
     <>
-      <h1 className="bg-blue-600">Todo</h1>
+      <div className="min-w-full h-screen">
+        <div className="">
+          <h1 className="font-mono text-4xl font-bold text-black">Tasks For The Day</h1>
+          <div>
+            <input type="text"
+              placeholder="Enter Task"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+              className="bg-white"
+            />
+            <button onClick={addTask} className="bg-blue-400 border-none"> Add Task</button>
+            <div>
+              {tasks.map((t,index) => {
+                return <p key={index} className="text-black">{t}</p>
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
